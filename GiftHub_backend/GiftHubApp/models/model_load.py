533664b06e9c110ce4adb55cce60d9d83d3cf57e
Sehoon <ksstpgns1@gmail.com>
2024-03-18 14:26:20 +0900

    [Feat] #22 추천 모델 관련 코드 추가

--- GiftHub_backend/GiftHubApp/models/model_load.py
+++ GiftHub_backend/GiftHubApp/models/model_load.py
@@ -6,8 +6,10 @@ from django.conf import settings
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(os.path.dirname(__file__)))))
 from mlartifacts.bert4rec.model import BERT4Rec, params
 
+# naver
 model_ca_proba = mlflow.pyfunc.load_model(settings.PATH_CATEGORY_PROBA)
 
+# amazon
 model_bert4rec = BERT4Rec(**params)
 model_bert4rec.load_state_dict(torch.load(os.path.join(settings.PATH_BERT4REC, "model.pt")))
 model_bert4rec.to(params["device"])
